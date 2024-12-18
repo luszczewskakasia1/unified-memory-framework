59fd16a059887f7c19ba8e6efb07369257f7c122
Katarzyna Luszczewska <katarzyna.luszczewska@intel.com>
2024-12-18 11:38:05 +0100

    add sys.exit

--- .github/scripts/run-codespell.py
+++ .github/scripts/run-codespell.py
@@ -1,12 +1,12 @@
 """
- Copyright (C) 2023-2024 Intel Corporation
+ Copyright (C) 2024 Intel Corporation
 
  Under the Apache License v2.0 with LLVM Exceptions. See LICENSE.TXT.
  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 """
 
 import subprocess  # nosec B404
-import os
+import sys
 
 
 def codespell_scan():
@@ -16,14 +16,14 @@ def codespell_scan():
                 "codespell",
                 "-H",
                 "--quiet-level=3",
-                "--skip=*.supp,./.venv,./.git,./.github/scripts/run-codespell.py,./.github/workflows/.spellcheck-conf.toml",
+                "--skip=*.supp,./.git,./.github/scripts/run-codespell.py,./.github/workflows/.spellcheck-conf.toml",
                 '--ignore-words-list="ASSER,Tne,ba,BA"',
             ],
             text=True,
-            shell=False,
         )
         if codespell_result.returncode != 0:
             print(codespell_result)
+            sys.exit(1)
         else:
             print("No spelling errors found")
     except subprocess.CalledProcessError as ex:
