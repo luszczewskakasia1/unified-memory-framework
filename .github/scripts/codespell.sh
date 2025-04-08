8ea8fe4f4b50def96e1078811c066bca4e3c43e5
luszczewskakasia1 <katarzyna.luszczewska@intel.com>
2025-04-08 12:21:12 +0000

    test runner

--- .github/scripts/codespell.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-SCAN_RESULT=`codespell -H --quiet-level=3 --skip="*.h,*.cmake,*.c,*.hpp,*.cpp,*.sh,*.py,test/supp/*.supp,./.venv" --ignore-words-list="ASSER,Tne,ba,BA"`
-echo -e "${SCAN_RESULT}" 
